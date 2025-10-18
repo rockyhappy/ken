07e623d3a8aa99646ba89563101a135c9fb1a9ce
rockyhappy <rachit22153019@akgec.ac.in>
2025-10-19 01:07:03 +0530

    Prod Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.devrachit.ken"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.7"
+        versionCode = 11
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
